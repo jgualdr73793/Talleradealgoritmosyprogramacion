cc8aecb037a38370eabb96cc3a2516bcc0a49ae8
Juan Felipe Gualdria Carranza <102546313+jgualdr73793@users.noreply.github.com>
2022-05-01 21:06:26 -0500

    Delete Taller de Estrucuras de Control Repeticion-2 directory

--- Taller de Estrucuras de Control Repeticion-2/ejercicio_2.py
+++
@@ -1,4 +0,0 @@
-for c in range(1,101,2):
-    if c%7!=0:
-        print(c)
-    
--- Taller de Estrucuras de Control Repeticion-2/ejercicio_7.py
+++
@@ -1,9 +0,0 @@
-while True:
-    XM=input()
-    X, M=XM.split(" ")
-    X=int(X)
-    M=int(M)
-    if (X == 0 and M == 0):
-        break
-    E=X*M
-    print(E)
