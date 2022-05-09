d0669728a8770674643c1846d0c065919ef8b7ef
Juan Felipe Gualdria Carranza <102546313+jgualdr73793@users.noreply.github.com>
2022-05-09 13:51:21 -0500

    Add files via upload

---
+++ talleres jupyter/pruebas.py
@@ -0,0 +1,16 @@
+def resultados_partidos(partidos:[(int, int)]) -> (int, int, int):
+    c=0
+    x=0
+    y=0
+    for (g,g2) in partidos:
+        
+   
+          
+
+
+
+
+
+# prueba de la función anterior
+assert (resultados_partidos([(1, 3), (0, 0), (4, 0), (5, 3), (2, 2), (4, 3), (1, 0), (1, 2), (0, 0), (3, 2), (3, 1), (7, 0), (0, 2), (3, 3), (4, 2), (3, 4)])) == (8, 4, 4)
+print("Prueba superada 💪🏽")
